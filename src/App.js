e06d45bc2bd840aa4c463bd91f02e21da027f431
ms12046-blip <ms12046@auca.kg>
2026-04-24 14:52:42 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import useProducts from './hooks/useProducts';
 import useDashboard from './hooks/useDashboard';
 import Filters from './components/Filters/Filters';
+import Stats from './components/Stats/Stats';
 import DataTable from './components/DataTable/DataTable';
 
 function App() {
@@ -15,7 +16,11 @@ function App() {
     sortBy,
     setSortBy,
     categories,
-    filteredProducts
+    filteredProducts,
+    totalProducts,
+    filteredCount,
+    averagePrice,
+    totalStock
   } = useDashboard(products)
 
   return (
@@ -29,6 +34,10 @@ function App() {
         <div className="app__content">
           <div className="app__section">
             <Filters search={search} setSearch={setSearch} category={category} setCategory={setCategory} sortBy={sortBy} setSortBy={setSortBy} categories={categories} />
+          </div>
+
+          <div className="app__section">
+            <Stats totalProducts={totalProducts} filteredCount={filteredCount} averagePrice={averagePrice} totalStock={totalStock} />
           </div>
 
           <div className="app__section">
